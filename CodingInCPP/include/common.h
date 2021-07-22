0e23c8dbee4a8f8b5b76bb38fecda6c11b7eb2c7
Zuixi <50632199+Zuixi@users.noreply.github.com>
2021-07-22 18:38:36 +0800

    添加共享类

--- CodingInCPP/include/common.h
+++ CodingInCPP/include/common.h
@@ -38,4 +38,47 @@ public:
     thread_guard& operator= (thread_guard const&) = delete;    // 禁用拷贝复制运算符
 };
 
+class hierarchical_mutex {
+    std::mutex internal_mutex;
+    unsigned long const hierarchical_value;
+    unsigned long previous_hierarchy_value;
+    static std::thread_local unsigned long this_thread_hierarchy_value;
+
+    void check_for_hierarchy_violation() {
+        if (this_thread_hierarchy_value <= hierarchical_value) 
+        {
+            throw std::logic_error("mutex hierarchy violated");
+        }
+    }
+
+    void update_hierarchy_value() {
+        previous_hierarchy_value = this_thread_hierarchy_value;
+        this_thread_hierarchy_value = hierarchical_value;
+    }
+
+public:
+    explicit hierarchical_mutex(unsigned long value): hierarchical_value(value), previous_hierarchy_value(0) {}
+
+    void lock() {
+        check_for_hierarchy_violation();
+        internal_mutex.lock();
+        update_hierarchy_value();
+    }
+
+    void unlock() {
+        this_thread_hierarchy_value = previous_hierarchy_value;
+        internal_mutex.unlock();
+    }
+
+    bool try_lock() {
+        check_for_hierarchy_violation();
+        
+        if (!internal_mutex.try_lock())
+            return false;
+        
+        update_hierarchy_value();
+        return true;
+    }
+};
+
 #endif
